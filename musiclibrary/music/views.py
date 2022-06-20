2a7bb7564fa033c90ff0abbdf795c4da30e4d8c1
Trog19 <tsrogers8@msn.com>
2022-06-20 08:58:12 -0600

    Created urls.py file in /music + added code for URL pathways + created SongList class + post and get functions

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -1,3 +1,24 @@
+from operator import truediv
 from django.shortcuts import render
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.views import APIView  
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+class SongList(APIView):
+
+        def get(self, request):
+            song = Song.objects.all()
+            serializer = SongSerializer(song, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK) #make 200_success
+
+        def post(self, request):
+            serializer = SongSerializer(data=request.data)
+            if serializer.is_valid( ):
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        def put
